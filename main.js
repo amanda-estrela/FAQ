268e54417bc675ae33541ebf0a1b8f077b0f9839
Amanda Estrela <efernandes.amanda@gmail.com>
2022-02-17 01:56:31 -0300

    Correção de classes e estilos

--- main.js
+++ main.js
@@ -1,1 +1,15 @@
-document.querySelector('.resposta')
+const answer = document.querySelectorAll('.resposta')
+const botaoSeta = document.querySelectorAll('.pergunta__seta')
+
+
+/*botaoSeta.addEventListener('click', () => {
+    answer.classList.remove('resposta');
+    answer.classList.add('respostaAtiva');
+   }
+)*/
+
+/*for (let contador = 0; contador < answer.length; contador++){
+    botaoSeta[contador].addEventListener('click', () =>{
+        answer[contador].classList.toggle(respostaAtiva);
+    })
+}*/
