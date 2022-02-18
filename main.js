a5185775ecc4e2e5ea8ffc2c16d834d21d409970
Amanda Estrela <efernandes.amanda@gmail.com>
2022-02-18 13:14:43 -0300

    Correção do icone da seta

--- main.js
+++ main.js
@@ -1,13 +1,13 @@
 const answer = document.querySelectorAll('.resposta')
-const question = document.querySelectorAll('.pergunta', '.pergunta__seta')
+const question = document.querySelectorAll('.pergunta')
 const iconeSeta = document.querySelectorAll('.pergunta__seta')
 
 for (let contador = 0; contador < question.length; contador++){
     question[contador].addEventListener('click', () =>{
     answer[contador].classList.toggle('respostaAtiva');
     question[contador].classList.toggle('perguntaAtiva');
-    iconeSeta[contador].classList.toggle('.pergunta__setaAtiva');
-     })
+    iconeSeta[contador].classList.toggle('pergunta__setaAtiva');
+    })
 }
 
 
