10b681fd38f9196f07d07f0569cc05d079b0c4b5
Amanda Estrela <efernandes.amanda@gmail.com>
2022-02-20 11:02:01 -0300

    Ajuste da imagem mobile e do evento JS

--- main.js
+++ main.js
@@ -1,9 +1,10 @@
 const answer = document.querySelectorAll('.resposta')
 const question = document.querySelectorAll('.pergunta')
 const iconeSeta = document.querySelectorAll('.pergunta__seta')
+const faq = document.querySelectorAll('.questionario__unidade')
 
 for (let contador = 0; contador < question.length; contador++){
-    question[contador].addEventListener('click', () =>{
+    faq[contador].addEventListener('click', () =>{
     answer[contador].classList.toggle('respostaAtiva');
     question[contador].classList.toggle('perguntaAtiva');
     iconeSeta[contador].classList.toggle('pergunta__setaAtiva');
